99e0efae121a70b2db27d363863ba0e6c59506bf
zohaibjahandad <114934843+zohaibjahandad@users.noreply.github.com>
2022-10-17 18:48:49 +0500

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('zohaibjahandad')
+            node / gitConfigEmail('zohaib.jahandad@eurustechnologies.com')
         }
     }
     triggers {
